d82436aa7a349f8062425abf592529ce97a3c01e
Rinzii <ianpike98@gmail.com>
2024-02-28 23:46:27 -0500

    Begin implementing benchmarking

---
+++ ext/ccmath_benchmark_main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024-Present Ian Pike
+ * Copyright (c) 2024-Present ccmath contributors
+ *
+ * This library is provided under the MIT License.
+ * See LICENSE for more information.
+ */
+
+int main(int argc, char** argv)
+{
+}
