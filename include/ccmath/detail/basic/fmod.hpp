c3f104beb1a388b0185a3e9141506d7d86152ac1
Rinzii <ianpike98@gmail.com>
2024-02-28 20:18:37 -0500

    Simplify fmods logic

--- include/ccmath/detail/basic/fmod.hpp
+++ include/ccmath/detail/basic/fmod.hpp
@@ -8,58 +8,38 @@
 
 #pragma once
 
-//#include <type_traits>
+// #include <type_traits>
 #include <limits>
 
-#include "ccmath/detail/compare/isnan.hpp"
 #include "ccmath/detail/compare/isfinite.hpp"
-#include "ccmath/detail/nearest/trunc.hpp"
+#include "ccmath/detail/compare/isinf.hpp"
+#include "ccmath/detail/compare/isnan.hpp"
 #include "ccmath/detail/compare/signbit.hpp"
+#include "ccmath/detail/nearest/trunc.hpp"
 
 namespace ccm
 {
 	namespace
-    {
-        namespace impl
-        {
-			template <typename T, std::enable_if_t<std::is_floating_point_v<T>, int> = 0>
-			inline constexpr T fmod_impl_calculate(T x, T y)
-			{
-				// Calculate the remainder of the division of x by y.
-                return x - (ccm::trunc<T>(x / y) * y);
-			}
-
-			template <typename T, std::enable_if_t<std::is_integral_v<T>, int> = 0>
-			inline constexpr T fmod_impl_calculate(T x, T y)
-			{
-				// Calculate the remainder of the division of x by y.
-				// static_cast is required to prevent the compiler from complaining about narrowing with integer types.
-				return static_cast<T>(x - (ccm::trunc<T>(x / y) * y));
-			}
-
-			// Special case for floating point types
+	{
+		namespace impl
+		{
+			// NOTE: We do not raise FE_INVALID even is situations where the standard demands it.
 			template <typename T>
 			inline constexpr T fmod_impl_check(T x, T y) noexcept
 			{
-				// NOTE: We do not raise FE_INVALID even is situations where the standard demands it.
+				// Special edge cases for floating-point types.
 				if constexpr (std::numeric_limits<T>::is_iec559)
 				{
 					// If x is ±0 and y is not zero, ±0 is returned.
-					if ((x == static_cast<T>(0.0) || static_cast<T>(x) == static_cast<T>(-0.0)) && (y != static_cast<T>(0.0)))
+					if (x == static_cast<T>(0.0) && (y != static_cast<T>(0.0)))
 					{
 						// The standard specifies that plus or minus 0 is returned depending on the sign of x.
-						if (ccm::signbit(x))
-						{
-							return -static_cast<T>(0.0);
-						}
-						else
-						{
-							return static_cast<T>(0.0);
-						}
+						if (ccm::signbit(x)) { return -static_cast<T>(0.0); }
+						else { return static_cast<T>(0.0); }
 					}
 
-					// If x is ±∞ and y is not NaN, NaN is returned.
-					if ((x == +std::numeric_limits<T>::infinity() || x == -std::numeric_limits<T>::infinity()) && !ccm::isnan(y))
+					// If x is ±∞ and y is not NaN OR if y is ±0 and x is not NaN, -NaN is returned
+					if ((ccm::isinf(x) && !ccm::isnan(y)) || (y == static_cast<T>(0.0) && !ccm::isnan(x)))
 					{
 						// For some reason all the major compilers return a negative NaN even though I can't find anywhere
 						// in the standard that specifies this. I'm going to follow suit and return a negative NaN for now.
@@ -67,18 +47,8 @@ namespace ccm
 						return -std::numeric_limits<T>::quiet_NaN();
 					}
 
-					// If y is ±0 and x is not NaN, NaN is returned.
-					if ((y == static_cast<T>(0.0) || static_cast<T>(y) == static_cast<T>(-0.0)) && !ccm::isnan(x))
-					{
-						// Same issue as before. All major compilers return a negative NaN.
-						return -std::numeric_limits<T>::quiet_NaN();
-					}
-
 					// If y is ±∞ and x is finite, x is returned.
-					if ((y == +std::numeric_limits<T>::infinity() || y == -std::numeric_limits<T>::infinity()) && ccm::isfinite(x))
-					{
-						return x;
-					}
+					if (ccm::isinf(y) && ccm::isfinite(x)) { return x; }
 
 					// If either argument is NaN, NaN is returned.
 					if (ccm::isnan(x) || ccm::isnan(y))
@@ -88,7 +58,9 @@ namespace ccm
 					}
 				}
 
-				return fmod_impl_calculate(x, y);
+				// Calculate the remainder of the division of x by y.
+				// static_cast is required to prevent the compiler from complaining about narrowing with integer types.
+				return static_cast<T>(x - (ccm::trunc<T>(x / y) * y));
 			}
 
 			template <typename T, typename U, typename TC = std::common_type_t<T, U>>
@@ -97,8 +69,8 @@ namespace ccm
 				return fmod_impl_check(static_cast<TC>(x), static_cast<TC>(y));
 			}
 
-        }
-    }
+		} // namespace impl
+	}	  // namespace
 
 	/**
 	 * @brief Returns the floating-point remainder of the division operation x/y.
@@ -109,10 +81,10 @@ namespace ccm
 	 * @return The floating-point remainder of the division operation x/y.
 	 */
 	template <typename Real, std::enable_if_t<std::is_floating_point_v<Real>, int> = 0>
-    inline constexpr Real fmod(Real x, Real y)
-    {
-        return impl::fmod_impl_check(x, y);
-    }
+	inline constexpr Real fmod(Real x, Real y)
+	{
+		return impl::fmod_impl_check(x, y);
+	}
 
 	template <typename Integer, std::enable_if_t<std::is_integral_v<Integer>, int> = 0>
 	inline constexpr double fmod(Integer x, Integer y)
@@ -127,13 +99,13 @@ namespace ccm
 	}
 
 	inline constexpr float fmodf(float x, float y)
-    {
-        return fmod<float>(x, y);
-    }
+	{
+		return fmod<float>(x, y);
+	}
 
 	inline constexpr long double fmodl(long double x, long double y)
-    {
-        return fmod<long double>(x, y);
-    }
+	{
+		return fmod<long double>(x, y);
+	}
 
 } // namespace ccm
