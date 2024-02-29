26adec58e0de9d2e32c91a9ee9de1710ec80e2ac
Rinzii <ianpike98@gmail.com>
2024-02-28 20:17:52 -0500

    Add fmax support

--- test/basic/max_test.cpp
+++ test/basic/max_test.cpp
@@ -8,12 +8,32 @@
 
 #include <gtest/gtest.h>
 
-#include <ccmath/detail/basic/fdim.hpp>
+#include <ccmath/detail/basic/max.hpp>
 #include <cmath>
 #include <limits>
 
 
-TEST(CcmathBasicTests, Max)
+TEST(CcmathBasicTests, Fmax)
 {
+    EXPECT_EQ(ccm::fmax(1.0, 2.0), std::fmax(1.0, 2.0));
+    EXPECT_EQ(ccm::fmax(2.0, 1.0), std::fmax(2.0, 1.0));
+    EXPECT_EQ(ccm::fmax(1.0, 1.0), std::fmax(1.0, 1.0));
+    EXPECT_EQ(ccm::fmax(std::numeric_limits<double>::infinity(), 1.0), std::fmax(std::numeric_limits<double>::infinity(), 1.0));
+	EXPECT_EQ(ccm::fmax(1.0, std::numeric_limits<double>::infinity()), std::fmax(1.0, std::numeric_limits<double>::infinity()));
+	EXPECT_EQ(ccm::fmax(std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity()), std::fmax(std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity()));
+	EXPECT_EQ(ccm::fmax(-std::numeric_limits<double>::infinity(), 1.0), std::fmax(-std::numeric_limits<double>::infinity(), 1.0));
+	EXPECT_EQ(ccm::fmax(1.0, -std::numeric_limits<double>::infinity()), std::fmax(1.0, -std::numeric_limits<double>::infinity()));
+	EXPECT_EQ(ccm::fmax(-std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity()), std::fmax(-std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity()));
+	EXPECT_EQ(ccm::fmax(std::numeric_limits<double>::quiet_NaN(), 1.0), std::fmax(std::numeric_limits<double>::quiet_NaN(), 1.0));
+	EXPECT_EQ(ccm::fmax(1.0, std::numeric_limits<double>::quiet_NaN()), std::fmax(1.0, std::numeric_limits<double>::quiet_NaN()));
+
+	bool CcmFmaxWhenPassedPositiveNanReturnsNan = std::isnan(ccm::fmax(std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN()));
+	bool StdFmaxWhenPassedPositiveNanReturnsNan = std::isnan(std::fmax(std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN()));
+	EXPECT_EQ(CcmFmaxWhenPassedPositiveNanReturnsNan, StdFmaxWhenPassedPositiveNanReturnsNan);
+
+	bool CcmFmaxWhenPassedNegativeNanReturnsNan = std::isnan(ccm::fmax(-std::numeric_limits<double>::quiet_NaN(), -std::numeric_limits<double>::quiet_NaN()));
+	bool StdFmaxWhenPassedNegativeNanReturnsNan = std::isnan(std::fmax(-std::numeric_limits<double>::quiet_NaN(), -std::numeric_limits<double>::quiet_NaN()));
+	EXPECT_EQ(CcmFmaxWhenPassedNegativeNanReturnsNan, StdFmaxWhenPassedNegativeNanReturnsNan);
+
 
 }
