256f705e1a3db21e9b0e2dc2968f60badd2f07e6
Rinzii <ianpike98@gmail.com>
2024-02-29 19:28:37 -0500

    Finalize implementation and documentation

--- include/ccmath/detail/compare/isfinite.hpp
+++ include/ccmath/detail/compare/isfinite.hpp
@@ -8,28 +8,33 @@
 
 #pragma once
 
-#include <limits>
 #include "ccmath/detail/compare/isnan.hpp"
 #include "ccmath/detail/compare/isinf.hpp"
 
 namespace ccm
 {
-	namespace
-	{
-		namespace impl
-		{
-			template <typename T>
-			inline constexpr bool isfinite_impl(T num)
-			{
-				return (!ccm::isnan(num)) && (!ccm::isinf(num));
-			}
-		}
-	}
-
-	template <typename T>
+	/**
+	 * @brief Checks if the given number has a finite value.
+	 * @tparam T The type of the number.
+	 * @param num A floating-point or integer value
+	 * @return true if the number has a finite value, false otherwise.
+	 */
+	template <typename T, std::enable_if_t<!std::is_integral_v<T>, bool> = true>
 	inline constexpr bool isfinite(T num)
 	{
-		return impl::isfinite_impl(num);
+		return (!ccm::isnan(num)) && (!ccm::isinf(num));
 	}
+
+	/**
+     * @brief Checks if the given number has a finite value.
+     * @tparam Integer The type of the integer.
+     * @param num A integer value that will be converted to a double.
+     * @return true if the number has a finite value, false otherwise.
+     */
+	template <typename Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
+	inline constexpr bool isfinite(Integer num)
+    {
+        return isfinite<double>(static_cast<double>(num));
+    }
 
 } // namespace ccm
