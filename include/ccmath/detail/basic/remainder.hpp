2bb6ce836810753943adf348cdd44339bdeeeb88
Rinzii <ianpike98@gmail.com>
2024-02-29 19:21:49 -0500

    Improve documentation

--- include/ccmath/detail/basic/remainder.hpp
+++ include/ccmath/detail/basic/remainder.hpp
@@ -20,7 +20,7 @@ namespace ccm
 	 * @tparam T Type of the values to compare.
 	 * @param x Dividend.
 	 * @param y Divisor.
-	 * @return
+	 * @return The remainder of the division of x by y.
 	 */
 	template <typename T, std::enable_if_t<!std::is_integral<T>::value, int> = 0>
 	inline constexpr T remainder(T x, T y)
@@ -32,13 +32,21 @@ namespace ccm
 			// If either argument is NaN, NaN is returned.
 			if ((ccm::isinf(x) && !ccm::isnan(y)) || (y == 0 && !ccm::isnan(x)) || (ccm::isnan(x) || ccm::isnan(y)))
 			{
-				return std::numeric_limits<T>::quiet_NaN();
+				// All major compilers return -NaN.
+				return -std::numeric_limits<T>::quiet_NaN();
 			}
 		}
 
 		return static_cast<T>(x - (ccm::trunc<T>(x / y) * y));
 	}
 
+	/**
+     * @brief Returns the remainder of the division of x by y.
+     * @tparam Integer Type of the values to compare.
+     * @param x Dividend.
+     * @param y Divisor.
+     * @return The remainder of the division of x by y as a double.
+     */
 	template <typename Integer, std::enable_if_t<std::is_integral<Integer>::value, int> = 0>
 	inline constexpr double remainder(Integer x, Integer y)
 	{
