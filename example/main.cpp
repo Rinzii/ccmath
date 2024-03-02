8efb414c0d0b380f3de70d0208b738966e21f694
Rinzii <ianpike98@gmail.com>
2024-03-02 16:52:49 -0500

    Cleanup main for 0.1.0 release

--- example/main.cpp
+++ example/main.cpp
@@ -6,43 +6,11 @@
  * See LICENSE for more information.
  */
 
-//#include <cmath>
-#include <iostream>
-
-#include "ccmath/ccmath.hpp"
-
-#include <cmath>
-
 
 int main()
 {
 
-
-// EXPECT_EQ(ccm::signbit(-0.0), std::signbit(-0.0));
-
-    constexpr double a = -0.0;
-	constexpr double val = ccm::signbit(a);
-	static_assert(val == true, "ccm::signbit(-0.0) != true");
-
-	std::remquo(1.0, 2.0, nullptr);
-	__builtin_remquo(1.0, 2.0, nullptr);
-	// 	EXPECT_EQ(ccm::trunc(-std::numeric_limits<double>::quiet_NaN()), std::trunc(-std::numeric_limits<double>::quiet_NaN()));
-
-	constexpr double b = -std::numeric_limits<double>::quiet_NaN();
-	constexpr double val2 = ccm::trunc(b);
-	static_assert(ccm::isnan(val2), "ccm::trunc(-std::numeric_limits<double>::quiet_NaN()) != std::trunc(-std::numeric_limits<double>::quiet_NaN())");
-
-	// Test constexpr remquo
-	constexpr double x = 1.0;
-	constexpr double y = 2.0;
-	static constexpr int * quo = nullptr;
-	constexpr double result = ccm::remquo(x, y, quo);
-
-    // Test trunc
-    double b1 = std::trunc(-std::numeric_limits<double>::quiet_NaN());
-
-
-	double b2 = ccm::trunc(-std::numeric_limits<double>::quiet_NaN());
+	// TODO: Implement actual examples showcasing the library
 
     return 0;
 }
