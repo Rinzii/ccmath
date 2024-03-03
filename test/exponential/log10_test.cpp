737f4d38a8157675522e104631c0d91468a11186
Rinzii <ianpike98@gmail.com>
2024-03-02 19:45:11 -0500

    Add boilerplate code for future unit tests

---
+++ test/exponential/log10_test.cpp
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2024-Present Ian Pike
+* Copyright (c) 2024-Present ccmath contributors
+*
+* This library is provided under the MIT License.
+* See LICENSE for more information.
+*/
+
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <limits>
+#include "ccmath/detail/exponential/log10.hpp"
+
+TEST(CcmathExponentialTests, Log10)
+{
+
+}
