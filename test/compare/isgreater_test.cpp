0da545564518fc337f07c4dbd42ba5eaecd78384
Rinzii <ianpike98@gmail.com>
2024-03-02 19:15:53 -0500

    Add test cases for all new compare functions

---
+++ test/compare/isgreater_test.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024-Present Ian Pike
+ * Copyright (c) 2024-Present ccmath contributors
+ *
+ * This library is provided under the MIT License.
+ * See LICENSE for more information.
+ */
+
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <limits>
+#include "ccmath/detail/compare/isgreater.hpp"
+
+TEST(CcmathCompareTests, IsGreater)
+{
+	// Test the basic functionality of isgreater
+    EXPECT_EQ(ccm::isgreater(1.0, 0.0), std::isgreater(1.0, 0.0));
+	EXPECT_EQ(ccm::isgreater(0.0, 1.0), std::isgreater(0.0, 1.0));
+	EXPECT_EQ(ccm::isgreater(0.0, 0.0), std::isgreater(0.0, 0.0));
+	EXPECT_EQ(ccm::isgreater(1.0, 1.0), std::isgreater(1.0, 1.0));
+	EXPECT_EQ(ccm::isgreater(-1.0, 0.0), std::isgreater(-1.0, 0.0));
+	EXPECT_EQ(ccm::isgreater(0.0, -1.0), std::isgreater(0.0, -1.0));
+	EXPECT_EQ(ccm::isgreater(-1.0, -1.0), std::isgreater(-1.0, -1.0));
+}
