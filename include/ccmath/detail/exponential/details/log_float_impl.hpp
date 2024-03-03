9c560f28f2866885c1f48c6478b3e76871c96d67
Rinzii <ianpike98@gmail.com>
2024-03-03 09:20:34 -0500

    cleanup of log details and add more comments.

--- include/ccmath/detail/exponential/details/log_float_impl.hpp
+++ include/ccmath/detail/exponential/details/log_float_impl.hpp
@@ -7,3 +7,27 @@
  */
 
 #pragma once
+
+
+#include "ccmath/detail/compare/isnan.hpp"
+
+#include <array>
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+#include "ccmath/internal/predef/unlikely.hpp"
+
+
+
+namespace ccm::internal
+{
+	namespace
+	{
+		namespace impl
+		{
+			// TODO: Implement float version of log
+			// TODO: Decide if we instead just default to double version always.
+		} // namespace impl
+    } // namespace
+} // namespace ccm::internal
