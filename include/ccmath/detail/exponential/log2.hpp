4de20f65b392d02cfb453c0dfaddaa5e7c8b8e4c
Rinzii <ianpike98@gmail.com>
2024-03-04 07:43:33 -0500

    Initial working revision of log2

--- include/ccmath/detail/exponential/log2.hpp
+++ include/ccmath/detail/exponential/log2.hpp
@@ -12,6 +12,15 @@
 
 #include <type_traits>
 
+#include <array>
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+#include "ccmath/internal/helpers/bits.hpp"
+#include "ccmath/internal/helpers/floating_point_type.hpp"
+#include "ccmath/internal/predef/unlikely.hpp"
+
 namespace ccm
 {
 	//template
