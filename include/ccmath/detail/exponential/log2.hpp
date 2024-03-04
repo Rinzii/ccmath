22fba25f9d03117ffb174910df0ca12572cd9deb
Rinzii <ianpike98@gmail.com>
2024-03-04 09:18:43 -0500

    Push current work. Need to revise log2 to resolve bugs later.

--- include/ccmath/detail/exponential/log2.hpp
+++ include/ccmath/detail/exponential/log2.hpp
@@ -11,18 +11,66 @@
 #include "ccmath/detail/compare/isnan.hpp"
 
 #include <type_traits>
-
-#include <array>
-#include <cstdint>
 #include <limits>
-#include <type_traits>
 
-#include "ccmath/internal/helpers/bits.hpp"
-#include "ccmath/internal/helpers/floating_point_type.hpp"
-#include "ccmath/internal/predef/unlikely.hpp"
+#include "ccmath/detail/compare/signbit.hpp"
+
+#include "ccmath/detail/exponential/details/log2_double_impl.hpp"
+#include "ccmath/detail/exponential/details/log2_float_impl.hpp"
 
 namespace ccm
 {
-	//template
+	template <typename T>
+	inline constexpr T log2(T num) noexcept
+	{
+		// If the argument is ±0, -∞ is returned
+		if (num == static_cast<T>(0))
+		{
+			return -std::numeric_limits<T>::infinity();
+		}
+
+		// If the argument is 1, +0 is returned.
+		if (num == static_cast<T>(1))
+		{
+			return 0;
+		}
+
+		// If the argument is negative, NaN is returned
+		if (ccm::signbit(num))
+		{
+			return std::numeric_limits<T>::quiet_NaN();
+		}
+
+		// If the argument is +∞, +∞ is returned.
+		if (num == std::numeric_limits<T>::infinity())
+		{
+			return std::numeric_limits<T>::infinity();
+		}
+
+		// If the argument is NaN, NaN is returned.
+		if (ccm::isnan(num))
+		{
+			return std::numeric_limits<T>::quiet_NaN();
+		}
+
+		// TODO: implement the float specific version of this.
+		return ccm::internal::log2_double(num);
+	}
+
+	template <typename Integer, std::enable_if_t<std::is_integral_v<Integer>, int> = 0>
+    inline constexpr double log2(Integer num) noexcept
+    {
+        return ccm::log2(static_cast<double>(num));
+    }
+
+	inline constexpr float log2f( float num )
+	{
+		return ccm::log2( num );
+	}
+
+	inline constexpr long double log2l( long double num )
+    {
+        return ccm::log2( num );
+    }
 
 } // namespace ccm
--- test/exponential/log2_test.cpp
+++ test/exponential/log2_test.cpp
@@ -14,5 +14,18 @@
 
 TEST(CcmathExponentialTests, Log2)
 {
+	EXPECT_EQ(ccm::log2(1.0), std::log2(1.0));
+	EXPECT_EQ(ccm::log2(2.0), std::log2(2.0));
+	EXPECT_EQ(ccm::log2(4.0), std::log2(4.0));
+	EXPECT_EQ(ccm::log2(8.0), std::log2(8.0));
+	EXPECT_EQ(ccm::log2(16.0), std::log2(16.0));
+	EXPECT_EQ(ccm::log2(32.0), std::log2(32.0));
+	EXPECT_EQ(ccm::log2(64.0), std::log2(64.0));
+	EXPECT_EQ(ccm::log2(128.0), std::log2(128.0));
+	EXPECT_EQ(ccm::log2(256.0), std::log2(256.0));
+	EXPECT_EQ(ccm::log2(512.0), std::log2(512.0));
+	EXPECT_EQ(ccm::log2(1024.0), std::log2(1024.0));
+	EXPECT_EQ(ccm::log2(2048.0), std::log2(2048.0));
+	EXPECT_EQ(ccm::log2(4096.0), std::log2(4096.0));
 
 }
