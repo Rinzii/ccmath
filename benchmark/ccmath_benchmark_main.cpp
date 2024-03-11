07dc8d8efa220ebca846d9398762c3e2fabb7d0c
Rinzii <ianpike98@gmail.com>
2024-03-11 18:51:51 -0400

    Implement basic benchmarking for log and log2

--- benchmark/ccmath_benchmark_main.cpp
+++ benchmark/ccmath_benchmark_main.cpp
@@ -6,6 +6,37 @@
  * See LICENSE for more information.
  */
 
-int main(int argc, char** argv)
-{
+#include <iostream>
+#include <cmath>
+#include "ccmath/ccmath.hpp"
+#include <benchmark/benchmark.h>
+
+static void BM_ccm_log2(benchmark::State& state) {
+	for (auto _ : state) {
+		benchmark::DoNotOptimize(ccm::log2(state.range(0)));
+	}
 }
+BENCHMARK(BM_ccm_log2)->Arg(16)->Arg(256)->Arg(4096)->Arg(65536);
+
+static void BM_ccm_log(benchmark::State& state) {
+	for (auto _ : state) {
+		benchmark::DoNotOptimize(ccm::log(state.range(0)));
+	}
+}
+BENCHMARK(BM_ccm_log)->Arg(16)->Arg(256)->Arg(4096)->Arg(65536);
+
+static void BM_std_log(benchmark::State& state) {
+	for (auto _ : state) {
+		benchmark::DoNotOptimize(std::log(state.range(0)));
+	}
+}
+BENCHMARK(BM_std_log)->Arg(16)->Arg(256)->Arg(4096)->Arg(65536);
+
+static void BM_std_log2(benchmark::State& state) {
+	for (auto _ : state) {
+		benchmark::DoNotOptimize(std::log2(state.range(0)));
+	}
+}
+BENCHMARK(BM_std_log2)->Arg(16)->Arg(256)->Arg(4096)->Arg(65536);
+
+BENCHMARK_MAIN();
