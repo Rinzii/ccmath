a7c17209f52d0868ecd0106abf06186b86a2835d
Rinzii <ianpike98@gmail.com>
2024-03-12 17:29:04 -0400

    Add support for Nvidia HPC and remove use of enums.

--- include/ccmath/internal/helpers/fpclassify_helper.hpp
+++ include/ccmath/internal/helpers/fpclassify_helper.hpp
@@ -25,35 +25,36 @@
 
 namespace ccm::helpers
 {
-	enum class floating_point_defines
-    {
+	struct floating_point_defines
+	{
 #if defined(CCMATH_COMPILER_MSVC) // Mirrors the corecrt definitions
-        eFP_NAN = 2,
-		eFP_INFINITE = 1,
-		eFP_ZERO = 0,
-		eFP_SUBNORMAL = -2,
-		eFP_NORMAL = -1
+		constexpr static int eFP_NAN = 2;
+		constexpr static int eFP_INFINITE = 1;
+		constexpr static int eFP_ZERO = 0;
+		constexpr static int eFP_SUBNORMAL = -2;
+		constexpr static int eFP_NORMAL = -1;
 #elif defined(CCMATH_COMPILER_APPLE_CLANG) // Apple Clang has a different set of defines than Clang
-        eFP_NAN = 1,
-        eFP_INFINITE = 2,
-        eFP_ZERO = 3,
-        eFP_SUBNORMAL = 5,
-        eFP_NORMAL = 4,
+		constexpr static int eFP_NAN = 1;
+		constexpr static int eFP_INFINITE = 2;
+		constexpr static int eFP_ZERO = 3;
+		constexpr static int eFP_SUBNORMAL = 5;
+		constexpr static int eFP_NORMAL = 4;
+#elif defined(CCMATH_COMPILER_NVIDIA_HPC) // Nvidia HPC SDK has a different set of defines than Clang
+		constexpr static int eFP_NAN = 0;
+		constexpr static int eFP_INFINITE = 1;
+		constexpr static int eFP_ZERO = 2;
+		constexpr static int eFP_SUBNORMAL = 2;
+		constexpr static int eFP_NORMAL = -4;
 #elif defined(CCMATH_COMPILER_CLANG) || defined(CCMATH_COMPILER_GCC) || defined(CCMATH_COMPILER_CLANG_BASED) // Clang and GCC have the same defines
-        eFP_NAN = 0,
-        eFP_INFINITE = 1,
-        eFP_ZERO = 2,
-        eFP_SUBNORMAL = 3,
-        eFP_NORMAL = 4,
-#else // Unknown compiler throws a static_assert. Since FP_* macros are extremely implementation specific, we can't provide a default.
-        eFP_NAN = 0,
-        eFP_INFINITE,
-        eFP_ZERO,
-        eFP_SUBNORMAL,
-        eFP_NORMAL
+		constexpr static int eFP_NAN = 0;
+		constexpr static int eFP_INFINITE = 1;
+		constexpr static int eFP_ZERO = 2;
+		constexpr static int eFP_SUBNORMAL = 3;
+		constexpr static int eFP_NORMAL = 4;
+#else
 		static_assert(false, "FP_* macros are extremely implementation specific and are not defined for this compiler. Please add support for this compiler.")
 #endif
-    };
+	};
 }
 
 // Clean up the global namespace
