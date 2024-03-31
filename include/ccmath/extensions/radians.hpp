4ed8a6523e7d9cfc057c636a067a239cf8aca854
Rinzii <ianpike98@gmail.com>
2024-03-30 20:22:32 -0400

    Add useful function radians to extensions

---
+++ include/ccmath/extensions/radians.hpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024-Present Ian Pike
+ * Copyright (c) 2024-Present ccmath contributors
+ *
+ * This library is provided under the MIT License.
+ * See LICENSE for more information.
+ */
+
+#pragma once
+
+#include "ccmath/numbers.hpp"
+#include <type_traits>
+
+namespace ccm::ext
+{
+	template <typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
+	constexpr T radians(T degrees) noexcept
+	{
+		return (ccm::numbers::pi_v<T> * degrees) / static_cast<T>(180);
+	}
+}
