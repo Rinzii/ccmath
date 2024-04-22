1e5b055f51f8f85aa84760513fdcbbcd8c7a21f9
ian <ianpike98@gmail.com>
2024-04-21 22:45:03 -0400

    Start beginnings of function signature for exp2

--- include/ccmath/math/exponential/exp2.hpp
+++ include/ccmath/math/exponential/exp2.hpp
@@ -11,9 +11,19 @@
 namespace ccm
 {
 	template <typename T>
-	constexpr T exp2([[maybe_unused]] T x)
+	constexpr T exp2(T num)
 	{
-		return 0;
+#if defined(__GNUC__) && (__GNUC__ > 6 || (__GNUC__ == 6 && __GNUC_MINOR__ >= 1)) && !defined(__clang__)
+		if constexpr (std::is_same_v<T, float>) { return __builtin_expf(num); }
+		else if constexpr (std::is_same_v<T, double>) { return __builtin_exp(num); }
+		else if constexpr (std::is_same_v<T, long double>) { return __builtin_expl(num); }
+		else { return __builtin_exp(num); }
+#else // TODO: Implement generic exp2
+		if constexpr (std::is_same_v<T, float>) { return 0; }
+		else if constexpr (std::is_same_v<T, double>) { return 0; }
+		else if constexpr (std::is_same_v<T, long double>) { return 0; }
+		else { return 0; }
+#endif
 	}
 
 } // namespace ccm
