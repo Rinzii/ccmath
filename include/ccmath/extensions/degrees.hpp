b4a95bbe398153d2ba2549421d231c5e2040b338
ian <ianpike98@gmail.com>
2024-04-28 06:29:26 -0400

    Expand documentation of ext functions

--- include/ccmath/extensions/degrees.hpp
+++ include/ccmath/extensions/degrees.hpp
@@ -13,9 +13,15 @@
 
 namespace ccm::ext
 {
+	/**
+	 * @brief Converts an angle in radians to degrees.
+	 * @tparam T Floating-point type.
+	 * @param radians Angle in radians.
+	 * @return Angle in degrees.
+	 */
 	template <typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
     constexpr T degrees(T radians) noexcept
     {
         return (static_cast<T>(180) * radians) / ccm::numbers::pi_v<T>;
     }
-}
+} // namespace ccm::ext
