63f2cece84e03aeff1c25b6780e4854f58316517
ian <ianpike98@gmail.com>
2024-05-06 11:30:38 -0400

    Cleanup signbit

--- include/ccmath/math/compare/signbit.hpp
+++ include/ccmath/math/compare/signbit.hpp
@@ -75,9 +75,9 @@ namespace ccm
 		{
 			// If constexpr only works with gcc 7.1+. Without if constexpr we work till GCC 5.1+
 			// This works with clang 5.0.0 no problem even with if constexpr
-			if constexpr (std::is_same_v<T, float>) { return __builtin_copysignf(1.0f, x) < 0; }
+			if constexpr (std::is_same_v<T, float>) { return __builtin_copysignf(1.0F, x) < 0; }
 			else if constexpr (std::is_same_v<T, double>) { return __builtin_copysign(1.0, x) < 0; }
-			else if constexpr (std::is_same_v<T, long double>) { return __builtin_copysignl(1.0l, x) < 0; }
+			else if constexpr (std::is_same_v<T, long double>) { return __builtin_copysignl(1.0L, x) < 0; }
 
 			return false;
 		}
