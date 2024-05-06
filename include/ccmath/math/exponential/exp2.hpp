a4c094bad8af2f43e391866b76e5d46fa18d586d
ian <ianpike98@gmail.com>
2024-05-06 15:18:41 -0400

    Add missing header and fix incorrect call to wrong builtin function

--- include/ccmath/math/exponential/exp2.hpp
+++ include/ccmath/math/exponential/exp2.hpp
@@ -8,17 +8,19 @@
 
 #pragma once
 
+#include <type_traits>
+
 namespace ccm
 {
-	template <typename T>
+	template <typename T, std::enable_if_t<!std::is_integral_v<T>, bool> = true>
 	constexpr T exp2(T num)
 	{
 #if defined(__GNUC__) && (__GNUC__ > 6 || (__GNUC__ == 6 && __GNUC_MINOR__ >= 1)) && !defined(__clang__)
-		if constexpr (std::is_same_v<T, float>) { return __builtin_expf(num); }
-		else if constexpr (std::is_same_v<T, double>) { return __builtin_exp(num); }
-		else if constexpr (std::is_same_v<T, long double>) { return __builtin_expl(num); }
-		else { return __builtin_exp(num); }
-#else // TODO: Implement generic exp2
+		if constexpr (std::is_same_v<T, float>) { return __builtin_exp2f(num); }
+		else if constexpr (std::is_same_v<T, double>) { return __builtin_exp2(num); }
+		else if constexpr (std::is_same_v<T, long double>) { return __builtin_exp2l(num); }
+		else { return __builtin_exp2(num); }
+#else
 		if constexpr (std::is_same_v<T, float>) { return 0; }
 		else if constexpr (std::is_same_v<T, double>) { return 0; }
 		else if constexpr (std::is_same_v<T, long double>) { return 0; }
@@ -26,4 +28,19 @@ namespace ccm
 #endif
 	}
 
+	template <typename Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
+	constexpr double exp2(Integer num)
+	{
+		return ccm::exp2<double>(static_cast<double>(num));
+	}
+
+	constexpr float exp2f(float num)
+	{
+		return ccm::exp2<float>(num);
+	}
+
+	constexpr long double exp2l(double num)
+	{
+		return ccm::exp2<double>(num);
+	}
 } // namespace ccm
