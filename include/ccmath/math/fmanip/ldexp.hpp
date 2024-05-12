0ca9e5aab909a5d28610d1e024ee91e9b88117e3
Corey Williams <corey.w108@gmail.com>
2024-05-12 10:09:51 -0500

    requested changes

--- include/ccmath/math/fmanip/ldexp.hpp
+++ include/ccmath/math/fmanip/ldexp.hpp
@@ -20,29 +20,29 @@ namespace ccm
 	 * @param exp An integer value.
 	 * @return If no errors occur, num multiplied by 2 to the power of exp (num×2exp) is returned.\nIf a range error due to overflow occurs, ±HUGE_VAL, ±HUGE_VALF, or ±HUGE_VALL is returned.\nIf a range error due to underflow occurs, the correct result (after rounding) is returned.
 	 */
-	template <typename Floating_Point, std::enable_if_t<!std::is_integral_v<Floating_Point>, bool> = true>
-	inline constexpr Floating_Point ldexp(Floating_Point num, std::int32_t exp) noexcept
+	template <typename T, std::enable_if_t<!std::is_integral_v<T>, bool> = true>
+	inline constexpr /* floating-point-type */ T ldexp(/* floating-point-type */ T num, int exp) noexcept
 	{
-		std::int32_t oldexp = ccm::support::get_exponent_of_floating_point<Floating_Point>(num);
+		std::int32_t oldexp = ccm::support::get_exponent_of_floating_point<T>(num);
 		
 		// if the mantissa is 0 and the original exponent is 0, or infinite, return num
-		const auto bits = ccm::support::bit_cast<ccm::helpers::float_bits_t<Floating_Point>>(num);
+		const auto bits = ccm::support::bit_cast<ccm::helpers::float_bits_t<T>>(num);
 
 		if (CCM_UNLIKELY(!ccm::isfinite(num)) 
-			|| oldexp < ccm::helpers::floating_point_traits<Floating_Point>::minimum_binary_exponent
+			|| oldexp < ccm::helpers::floating_point_traits<T>::minimum_binary_exponent
 			|| ((oldexp == 0) && 
-				((bits & ccm::helpers::floating_point_traits<Floating_Point>::normal_mantissa_mask) == 0)))
+				((bits & ccm::helpers::floating_point_traits<T>::normal_mantissa_mask) == 0)))
 		{
 			return num;
 		}
 
-		if (exp > ccm::helpers::floating_point_traits<Floating_Point>::maximum_binary_exponent)
+		if (exp > ccm::helpers::floating_point_traits<T>::maximum_binary_exponent)
 		{
 			// error == hugeval
-			return std::numeric_limits<Floating_Point>::infinity();
+			return std::numeric_limits<T>::infinity();
 		}
 		// the reference source says -2 * exp_max
-		else if (exp < ccm::helpers::floating_point_traits<Floating_Point>::minimum_binary_exponent)
+		else if (exp < ccm::helpers::floating_point_traits<T>::minimum_binary_exponent)
 		{
 			// error == range
 			return 0;
@@ -50,25 +50,25 @@ namespace ccm
 		// normalizes an abnormal floating point
 		if (oldexp == 0)
 		{
-			num *= ccm::helpers::floating_point_traits<Floating_Point>::normalize_factor;
-			exp	   = -static_cast<std::int32_t>(sizeof(Floating_Point)) * std::numeric_limits<unsigned char>::digits; // bits in a byte
-			oldexp = ccm::support::get_exponent_of_floating_point<Floating_Point>(num);
+			num *= ccm::helpers::floating_point_traits<T>::normalize_factor;
+			exp	   = -static_cast<std::int32_t>(sizeof(T)) * std::numeric_limits<unsigned char>::digits; // bits in a byte
+			oldexp = ccm::support::get_exponent_of_floating_point<T>(num);
 		}
 
 		exp += oldexp;
-		if (exp >= ccm::helpers::floating_point_traits<Floating_Point>::maximum_binary_exponent)
+		if (exp >= ccm::helpers::floating_point_traits<T>::maximum_binary_exponent)
 		{
 			// overflow
-			return std::numeric_limits<Floating_Point>::infinity();
+			return std::numeric_limits<T>::infinity();
 		}
 		if (exp > 0) { 
-			return ccm::support::set_exponent_of_floating_point<Floating_Point>(num, exp);
+			return ccm::support::set_exponent_of_floating_point<T>(num, exp);
 		}
 		// denormal, or underflow
-		exp += static_cast<std::int32_t>(sizeof(Floating_Point)) * std::numeric_limits<unsigned char>::digits; // bits in a byte
-		num = ccm::support::set_exponent_of_floating_point<Floating_Point>(num, exp);
-		num /= ccm::helpers::floating_point_traits<Floating_Point>::normalize_factor;
-		//if (num == static_cast<Floating_Point>(0))
+		exp += static_cast<std::int32_t>(sizeof(T)) * std::numeric_limits<unsigned char>::digits; // bits in a byte
+		num = ccm::support::set_exponent_of_floating_point<T>(num, exp);
+		num /= ccm::helpers::floating_point_traits<T>::normalize_factor;
+		//if (num == static_cast<T>(0))
 		//{
 			// underflow report not currently being handled
 		//}
@@ -84,7 +84,7 @@ namespace ccm
 	 * ±HUGE_VALF, or ±HUGE_VALL is returned.\nIf a range error due to underflow occurs, the correct result (after rounding) is returned.
 	 */
 	template <typename Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
-	inline constexpr double ldexp(Integer num, std::int32_t exp) noexcept
+	inline constexpr double ldexp(Integer num, int exp) noexcept
 	{
 		return ldexp<double>(static_cast<double>(num), exp);
 	}
@@ -95,7 +95,7 @@ namespace ccm
 	 * @param exp An integer value.
 	 * @return The result of parameter num multiplied by 2 to the power of parameter exp.
 	 */
-	inline constexpr float ldexpf(float num, std::int32_t exp) noexcept
+	inline constexpr float ldexpf(float num, int exp) noexcept
 	{
 		return ldexp<float>(num, exp);
 	}
