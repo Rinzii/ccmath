526a9ec09042554836f80927b59d721c0f6a12b9
ian <ianpike98@gmail.com>
2024-05-11 22:38:56 -0400

    Cleanup namespace use to be better inline with the entire library

--- include/ccmath/math/fmanip/impl/scalbn_float_impl.hpp
+++ include/ccmath/math/fmanip/impl/scalbn_float_impl.hpp
@@ -15,39 +15,51 @@
 
 #include <cstdint>
 
-namespace ccm::internal::impl
+namespace ccm::internal
 {
-	constexpr float scalbn_float_impl(float arg, int exp) noexcept
+	namespace impl
 	{
-		ccm::float_t tmp = arg;
-
-		if (exp > 127)
+		constexpr float scalbn_float_impl(float arg, int exp) noexcept
 		{
-			tmp *= 0x1p127F;
-			exp -= 127;
+			ccm::float_t tmp = arg;
+
 			if (exp > 127)
 			{
 				tmp *= 0x1p127F;
 				exp -= 127;
-				exp = ccm::min(exp, 127);
+				if (exp > 127)
+				{
+					tmp *= 0x1p127F;
+					exp -= 127;
+					exp = ccm::min(exp, 127);
+				}
 			}
-		}
-		else if (exp < -126)
-		{
-			tmp *= 0x1p-126F * 0x1p24F;
-			exp += 126 - 24;
-			if (exp < -126)
+			else if (exp < -126)
 			{
 				tmp *= 0x1p-126F * 0x1p24F;
 				exp += 126 - 24;
-				exp = ccm::max(exp, -126);
+				if (exp < -126)
+				{
+					tmp *= 0x1p-126F * 0x1p24F;
+					exp += 126 - 24;
+					exp = ccm::max(exp, -126);
+				}
 			}
+
+			const std::uint32_t bits = ccm::support::bit_cast<std::uint32_t>(127 + exp) << 23;
+			arg						 = tmp * ccm::support::bit_cast<float>(bits);
+
+			return arg;
 		}
+	} // namespace impl
 
-		const std::uint32_t bits = ccm::support::bit_cast<std::uint32_t>(127 + exp) << 23;
-		arg						 = tmp * ccm::support::bit_cast<float>(bits);
-
-		return arg;
+	constexpr float scalbn_float(float arg, int exp) noexcept
+	{
+		return impl::scalbn_float_impl(arg, exp);
 	}
 
-} // namespace ccm::internal::impl
+	constexpr float scalbn_float(float arg, long exp) noexcept
+	{
+		return impl::scalbn_float_impl(arg, static_cast<int>(exp));
+	}
+} // namespace ccm::internal
