a1c466e60ee501f7328a69d3f53d16043d2804d9
ian <ianpike98@gmail.com>
2024-05-24 16:25:28 -0400

    cleanup code for fenv handling

--- include/ccmath/internal/support/fenv/aarch64/fenv_support_darwin.hpp
+++ include/ccmath/internal/support/fenv/aarch64/fenv_support_darwin.hpp
@@ -14,8 +14,6 @@
 #include "ccmath/internal/config/arch/targets/aarch64.hpp" // CCM_TARGET_ARCH_AARCH64
 
 #if defined(CCM_TARGET_ARCH_AARCH64) && defined(__APPLE__)
-
-
 #include "ccmath/internal/support/fp_bits.hpp"
 
 #include <arm_acle.h>
@@ -271,5 +269,4 @@ namespace ccm::support::fenv::internal
 		return 0;
 	}
 } // namespace ccm::support::fenv::internal
-
-#endif
+#endif // defined(CCM_TARGET_ARCH_AARCH64) && defined(__APPLE__)
