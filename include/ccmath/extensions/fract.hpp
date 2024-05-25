e6fca504a2a9199c0e15a3ab63976ef14b32d06b
Eczbek <eczbek.void@gmail.com>
2024-05-25 14:48:29 -0400

    Fix missing documentation

--- include/ccmath/extensions/fract.hpp
+++ include/ccmath/extensions/fract.hpp
@@ -13,9 +13,15 @@
 
 namespace ccm::ext
 {
-    template <typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
-    constexpr T fract(T x) noexcept
+	/**
+	 * @brief Returns the fractional part of a floating-point number.
+	 * @tparam T Type of the input and output.
+	 * @param x Value to get the fractional part of.
+	 * @return The fractional part of the input.
+	 */
+	template <typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
+	constexpr T fract(T x) noexcept
 	{
-        return x - ccm::support::toward_zero(x);
+		return x - ccm::support::toward_zero(x);
 	}
-} // namespace ccm
+} // namespace ccm::ext
