d3fccfc9fc40a9c129789c77a77f45e5a78701e0
ian <ianpike98@gmail.com>
2024-06-10 21:25:04 -0400

    minor cleanup

--- include/ccmath/math/basic/impl/nan_double_impl.hpp
+++ include/ccmath/math/basic/impl/nan_double_impl.hpp
@@ -10,6 +10,7 @@
 
 #include "ccmath/internal/helpers/digit_to_int.hpp"
 #include "ccmath/internal/support/bits.hpp"
+
 #include <cstdint>
 #include <limits>
 
@@ -55,7 +56,7 @@ namespace ccm::internal::impl
 
 		if (!has_hex_been_detected)
 		{
-			// Check that all of are characters are numbers. If we detect a non-number, return the default NaN.
+			// Check that all of are the characters are numbers. If we detect a non-number, return the default NaN.
 			for (std::size_t i = 0; arg[i] != '\0'; ++i) // NOLINT
 			{
 				if (arg[i] < '0' || arg[i] > '9')
@@ -71,10 +72,10 @@ namespace ccm::internal::impl
 			for (std::size_t i = 0; arg[i] != '\0'; ++i)
 			{
 				dbl_bits *= 16;
-				dbl_bits += static_cast<uint8_t>(ccm::helpers::digit_to_int(arg[i])); // Convert ASCII to numeric value
+				dbl_bits += static_cast<std::uint8_t>(ccm::helpers::digit_to_int(arg[i])); // Convert ASCII to numeric value
 				if (i >= 15)
 				{
-					dbl_bits %= static_cast<uint64_t>(1e18); // Wrap around for numbers larger than 8 digits
+					dbl_bits %= static_cast<std::uint64_t>(1e18); // Wrap around for numbers larger than 8 digits
 				}
 			}
 		}
@@ -84,10 +85,10 @@ namespace ccm::internal::impl
 			for (std::size_t i = 0; arg[i] != '\0'; ++i)
 			{
 				dbl_bits *= 10;
-				dbl_bits += static_cast<uint8_t>(arg[i] - '0'); // Convert ASCII to numeric value
+				dbl_bits += static_cast<std::uint8_t>(arg[i] - '0'); // Convert ASCII to numeric value
 				if (i >= 15)
 				{
-					dbl_bits %= static_cast<uint64_t>(1e18); // Wrap around for numbers larger than 8 digits
+					dbl_bits %= static_cast<std::uint64_t>(1e18); // Wrap around for numbers larger than 8 digits
 				}
 			}
 		}
