b25d101d83ad0cb883fc77a9e919691dd9095390
ian <ianpike98@gmail.com>
2024-06-18 01:51:49 -0400

    Apply minor formating and structure changes along with some micro-optimizations

--- include/ccmath/internal/generic/functions/power/sqrt_gen.hpp
+++ include/ccmath/internal/generic/functions/power/sqrt_gen.hpp
@@ -30,14 +30,6 @@ namespace ccm::gen
 			static constexpr bool value = false;
 		};
 
-		template <typename T>
-		constexpr void normalize(int & exponent, typename support::fp::FPBits<T>::storage_type & mantissa)
-		{
-			const int shift = support::countl_zero(mantissa) - (8 * static_cast<int>(sizeof(mantissa)) - 1 - support::fp::FPBits<T>::fraction_length);
-			exponent -= shift;
-			mantissa <<= shift;
-		}
-
 #if defined(CCM_TYPES_LONG_DOUBLE_IS_FLOAT80)
 		template <>
 		struct Is80BitLongDouble<long double>
@@ -45,6 +37,17 @@ namespace ccm::gen
 			static constexpr bool value = true;
 		};
 #endif
+
+		template <typename T>
+		constexpr bool Is80BitLongDouble_v = Is80BitLongDouble<T>::value;
+
+		template <typename T>
+		constexpr void normalize(int & exponent, typename support::fp::FPBits<T>::storage_type & mantissa)
+		{
+			const int shift = support::countl_zero(mantissa) - (8 * static_cast<int>(sizeof(mantissa)) - 1 - support::fp::FPBits<T>::fraction_length);
+			exponent -= shift;
+			mantissa <<= shift;
+		}
 
 #if defined(CCM_TYPES_LONG_DOUBLE_IS_FLOAT64)
 		template <>
@@ -63,27 +66,25 @@ namespace ccm::gen
 		}
 #endif
 
-		template <typename T>
-		constexpr bool Is80BitLongDouble_v = Is80BitLongDouble<T>::value;
-
 		namespace impl
 		{
 			namespace bit80
 			{
 				// This has to be defined for sqrt_impl to work as it still needs to see that this function exists
-				constexpr long double sqrt_calc_80bits(long double x);
+				static constexpr long double sqrt_calc_80bits(long double x);
 
 #if defined(CCM_TYPES_LONG_DOUBLE_IS_FLOAT80)
-				constexpr long double sqrt_calc_80bits(long double x)
+				static constexpr long double sqrt_calc_80bits(long double x)
 				{
 					using Bits				   = support::fp::FPBits<long double>;
 					using storage_type		   = typename Bits::storage_type;
 					constexpr storage_type one = static_cast<storage_type>(1) << Bits::fraction_length;
-					constexpr auto nan_type	   = Bits::quiet_nan().get_val();
 
 					const Bits bits(x);
 
 					if (bits == Bits::inf(types::Sign::POS) || bits.is_zero() || bits.is_nan()) { return x; }
+
+					constexpr auto nan_type = Bits::quiet_nan().get_val();
 					if (bits.is_neg()) { return nan_type; }
 
 					int x_exp			= bits.get_explicit_exponent();
@@ -129,8 +130,7 @@ namespace ccm::gen
 					}
 
 					// We perform one more iteration to ensure that the result is correctly rounded.
-					const auto lsb = static_cast<bool>(y & 1);
-					bool round_bit = false;
+					bool round_bit{false};
 					r <<= 2;
 					if (const storage_type tmp = (y << 2) + 1; r >= tmp)
 					{
@@ -141,6 +141,8 @@ namespace ccm::gen
 					// Append the exponent field.
 					x_exp = ((x_exp >> 1) + Bits::exponent_bias);
 					y |= (static_cast<storage_type>(x_exp) << (Bits::fraction_length + 1));
+
+					const auto lsb = static_cast<bool>(y & 1); // Least significant bit
 
 					switch (support::fenv::get_rounding_mode())
 					{
@@ -198,7 +200,7 @@ namespace ccm::gen
 					for (storage_type current_bit = one >> 1; current_bit; current_bit >>= 1)
 					{
 						r <<= 1;
-						storage_type const tmp = (y << 1) + current_bit; // 2*y(n - 1) + 2^(-n-1)
+						const storage_type tmp = (y << 1) + current_bit; // 2*y(n - 1) + 2^(-n-1)
 						if (r >= tmp)
 						{
 							r -= tmp;
@@ -211,7 +213,7 @@ namespace ccm::gen
 					CCM_SIMD_VECTORIZE for (storage_type current_bit = one >> 1; current_bit; current_bit >>= 1)
 					{
 						r <<= 1;
-						storage_type const tmp = (y << 1) + current_bit; // 2*y(n - 1) + 2^(-n-1)
+						const storage_type tmp = (y << 1) + current_bit; // 2 * y(n - 1) + 2^(-n-1)
 						if (r >= tmp)
 						{
 							r -= tmp;
@@ -221,12 +223,11 @@ namespace ccm::gen
 				}
 
 				// We perform one more iteration to ensure that the result is correctly rounded.
-				auto lsb	   = static_cast<bool>(y & 1); // Least significant bit
-				bool round_bit = false;
+				bool round_bit{false};
 
 				r <<= 2;
 
-				if (storage_type const tmp = (y << 2) + 1; r >= tmp)
+				if (const storage_type tmp = (y << 2) + 1; r >= tmp)
 				{
 					r -= tmp;
 					round_bit = true;
@@ -235,7 +236,8 @@ namespace ccm::gen
 				// Remove the hidden bit and append the exponent field.
 				x_exp = ((x_exp >> 1) + FPBits_t::exponent_bias);
 
-				y = (y - one) | (static_cast<storage_type>(x_exp) << FPBits_t::fraction_length);
+				y			   = (y - one) | (static_cast<storage_type>(x_exp) << FPBits_t::fraction_length);
+				const auto lsb = static_cast<bool>(y & 1); // Least significant bit
 
 				switch (support::fenv::get_rounding_mode())
 				{
@@ -261,16 +263,14 @@ namespace ccm::gen
 				else
 				{
 					// IEEE floating points formats.
-					using FPBits_t		   = support::fp::FPBits<T>;
-					constexpr auto flt_nan = FPBits_t::quiet_nan().get_val();
-
+					using FPBits_t = support::fp::FPBits<T>;
 					const FPBits_t bits(x);
-
-					// bits.uin
 
 					// lut_double_sqrt_first_256
 					//  Handle special cases where the bits are +Inf, ±0, or ±NaN
 					if (CCM_UNLIKELY(bits == FPBits_t::inf(types::Sign::POS) || bits.is_zero() || bits.is_nan())) { return x; }
+
+					constexpr auto flt_nan = FPBits_t::quiet_nan().get_val();
 
 					// Handle special cases where the bits are -Inf or -num
 					if (CCM_UNLIKELY(bits.is_neg())) { return -flt_nan; }
