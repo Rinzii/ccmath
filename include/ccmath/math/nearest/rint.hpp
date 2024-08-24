fd06b4f47edb54b4b3322ee6dcbb3b8e28e8f071
Kyle Hurd <kyle.hurd@wsu.edu>
2024-08-24 04:43:53 -0400

    Implements rint
    Functions implemented:
    
    - ccm::rint(float)
    - ccm::rint(double)
    - ccm::rint(long double)
    - ccm::rintl(long double)
    - ccm::rintf(float)
    
    - ccm::lrint(float)
    - ccm::lrint(double)
    - ccm::lrint(long double)
    - ccm::lrintl(long double)
    - ccm::lrintf(float)
    
    - ccm::llrint(float)
    - ccm::llrint(double)
    - ccm::llrint(long double)
    - ccm::llrintl(long double)
    - ccm::llrintf(long double)
    
    Tests written for:
    
    - Common and edge case conditions for `float`, `double`, and `long
      double` inputs.
    - Assert that FE_INVALID is raised for +/- infinity values and NaN
      values.
    - Asserts that functions can be evaluated at compile time.

--- include/ccmath/math/nearest/rint.hpp
+++ include/ccmath/math/nearest/rint.hpp
@@ -10,7 +10,239 @@
 
 #pragma once
 
+#include <ccmath/internal/support/fenv/fenv_support.hpp>
+#include <ccmath/internal/support/fenv/rounding_mode.hpp>
+#include <ccmath/internal/support/fp/directional_rounding_utils.hpp>
+#include <ccmath/math/compare/isinf.hpp>
+#include <ccmath/math/compare/isnan.hpp>
+#include <type_traits>
+
 namespace ccm
 {
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value (in floating-point format), using the current rounding mode. The library provides
+	 * overloads of std::rint for all cv-unqualified floating-point types as the type of the parameter num.
+	 * @tparam T The type of the number.
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	template <class T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
+	constexpr T rint(T num)
+	{
+		if (ccm::isinf(num) || ccm::isnan(num)) { ccm::support::fenv::raise_except_if_required(FE_INVALID); }
+		const auto rounding_mode{ccm::support::fenv::get_rounding_mode()};
+		return ccm::support::fp::directional_round(num, rounding_mode);
+	}
+
+	/**
+	 * @brief Additional overloads are provided for all integer types, which are treated as double.
+	 * @tparam Integer The type of the integral value.
+	 * @param num An integral value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	template <class Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
+	constexpr double rint(Integer num)
+	{
+		return static_cast<double>(num);
+	}
+
+	/**
+	 * @brief Additional overloads are provided for all integer types, which are treated as double.
+	 * @tparam Integer The type of the integral value.
+	 * @param num An integral value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	template <class Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
+	constexpr long lrint(Integer num)
+	{
+		return static_cast<long>(num);
+	}
+
+	/**
+	 * @brief Additional overloads are provided for all integer types, which are treated as double.
+	 * @tparam Integer The type of the integral value.
+	 * @param num An integral value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	template <class Integer, std::enable_if_t<std::is_integral_v<Integer>, bool> = true>
+	constexpr long long llrint(Integer num)
+	{
+		return static_cast<long long>(num);
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value (in floating-point format), using the current rounding mode. The library provides
+	 * overloads of std::rint for all cv-unqualified floating-point types as the type of the parameter num.
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr float rint(float num)
+	{
+		return ccm::rint<float>(num);
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value (in floating-point format), using the current rounding mode. The library provides
+	 * overloads of std::rint for all cv-unqualified floating-point types as the type of the parameter num.
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr double rint(double num)
+	{
+		return ccm::rint<double>(num);
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value (in floating-point format), using the current rounding mode. The library provides
+	 * overloads of std::rint for all cv-unqualified floating-point types as the type of the parameter num.
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long double rint(long double num)
+	{
+		return ccm::rint<long double>(num);
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value (in floating-point format), using the current rounding mode. The library provides
+	 * overloads of std::rint for all cv-unqualified floating-point types as the type of the parameter num.
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr float rintf(float num)
+	{
+		return ccm::rint<float>(num);
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value (in floating-point format), using the current rounding mode. The library provides
+	 * overloads of std::rint for all cv-unqualified floating-point types as the type of the parameter num.
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long double rintl(long double num)
+	{
+		return ccm::rint<long double>(num);
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long lrint(float num)
+	{
+		return static_cast<long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long lrint(double num)
+	{
+		if (ccm::isnan(num))
+		{
+			ccm::support::fenv::raise_except_if_required(FE_INVALID);
+			return std::numeric_limits<long>::min();
+		}
+		return static_cast<long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long lrint(long double num)
+	{
+		return static_cast<long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long lrintf(float num)
+	{
+		return static_cast<long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long lrintl(long double num)
+	{
+		return static_cast<long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long long llrint(float num)
+	{
+		return static_cast<long long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long long llrint(double num)
+	{
+		if (ccm::isnan(num))
+		{
+			ccm::support::fenv::raise_except_if_required(FE_INVALID);
+			return std::numeric_limits<long long>::min();
+		}
+		return static_cast<long long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long long llrint(long double num)
+	{
+		return static_cast<long long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long long llrintf(float num)
+	{
+		return static_cast<long long>(ccm::rint(num));
+	}
+
+	/**
+	 * @brief Rounds the floating-point argument num to an integer value, using the current rounding mode. The library provides overloads of std::lrint and
+	 * std::llrint for all cv-unqualified floating-point types as the type of the parameter num.(
+	 * @param num A floating-point value.
+	 * @return If no errors occur, the nearest integer value to num, according to the current rounding mode, is returned.
+	 */
+	constexpr long long llrintl(long double num)
+	{
+		return static_cast<long long>(ccm::rint(num));
+	}
 
 } // namespace ccm
