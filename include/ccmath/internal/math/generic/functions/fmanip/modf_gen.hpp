9292914076cf141dc7ec7bc905dc9a5547978d10
Ian <ianpike98@gmail.com>
2024-08-28 13:53:13 -0400

    add placeholder files for new generic folder

---
+++ include/ccmath/internal/math/generic/functions/fmanip/modf_gen.hpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024-Present Ian Pike
+ * Copyright (c) 2024-Present ccmath contributors
+ *
+ * This library is provided under the MIT License.
+ * See LICENSE for more information.
+ */
+
+#pragma once
+
+namespace ccm
+{
+	template <typename T>
+	constexpr T modf(T x, T* iptr) noexcept
+	{
+		return 0;
+	}
+} // namespace ccm
