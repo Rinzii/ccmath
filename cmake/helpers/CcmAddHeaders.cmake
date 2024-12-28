40714f8faa16d5fcc363e5f8fd0dc3b3c2741606
Ian <ianpike98@gmail.com>
2024-12-28 13:43:20 -0500

    Switch over to improved ccm_add_headers
    Signed-off-by: Ian <ianpike98@gmail.com>

--- cmake/helpers/CcmAddHeaders.cmake
+++ cmake/helpers/CcmAddHeaders.cmake
@@ -1,10 +1,10 @@
 # Helper function to add headers to the global source list
-function(ccm_add_headers)
-  # Append all the passed arguments (headers) to the target's sources
-  target_sources(${PROJECT_NAME} INTERFACE "$<BUILD_INTERFACE:${ARGV}>")
-endfunction()
+#function(ccm_add_headers)
+#  # Append all the passed arguments (headers) to the target's sources
+#  target_sources(${PROJECT_NAME} INTERFACE "$<BUILD_INTERFACE:${ARGV}>")
+#endfunction()
 
-function(ccm_add_headers_temp)
+function(ccm_add_headers)
   foreach(header IN LISTS ARGV)
     target_sources(${PROJECT_NAME} INTERFACE "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${header}>")
   endforeach()
