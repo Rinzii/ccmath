4af90020c6a5f0244071d387cbb6852757a77ce2
Ian <ianpike98@gmail.com>
2024-12-28 15:14:33 -0500

    Remove builtin checking for now
    Signed-off-by: Ian <ianpike98@gmail.com>

--- cmake/config/features/GetAllSupportedFeatures.cmake
+++ cmake/config/features/GetAllSupportedFeatures.cmake
@@ -1,4 +1,11 @@
-include(${CCMATH_SOURCE_DIR}/cmake/config/features/CheckAllSupportedBuiltinFeatures.cmake)
+# Common Support Builtins
+include(${CCMATH_SOURCE_DIR}/cmake/config/features/builtin/support/CheckBuiltinBitCastSupport.cmake)
+include(${CCMATH_SOURCE_DIR}/cmake/config/features/builtin/support/CheckBuiltinFmaSupport.cmake)
+include(${CCMATH_SOURCE_DIR}/cmake/config/features/builtin/math/compare/CheckBuiltinSignbitSupport.cmake)
+
+
+# TODO: Revisit this in a manner that is better. The current approach takes up too much time in the cmake.
+#include(${CCMATH_SOURCE_DIR}/cmake/config/features/CheckAllSupportedBuiltinFeatures.cmake)
 
 if (CCMATH_ENABLE_RUNTIME_SIMD)
   include(${CCMATH_SOURCE_DIR}/cmake/config/features/CheckAllSupportedSimdFeatures.cmake)
