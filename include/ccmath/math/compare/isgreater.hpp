05e42e2e661a9365ff93b176dc75794335bfdb48
Ian <ianpike98@gmail.com>
2024-12-30 16:09:50 -0500

    Update comparison functions to be more standard conforming
    Signed-off-by: Ian <ianpike98@gmail.com>

--- include/ccmath/math/compare/isgreater.hpp
+++ include/ccmath/math/compare/isgreater.hpp
@@ -13,7 +13,7 @@
 #include <type_traits>
 
 #include "ccmath/internal/math/generic/builtins/compare/isgreater.hpp"
-
+#include "ccmath/math/compare/isunordered.hpp"
 
 namespace ccm
 {
@@ -24,29 +24,26 @@ namespace ccm
 	 * @param y A floating-point or integer value.
 	 * @return true if the first argument is greater than the second, false otherwise.
 	 */
-	template <typename T>
+	template <typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
 	constexpr bool isgreater(T x, T y) noexcept
 	{
 		if constexpr (ccm::builtin::has_constexpr_isgreater<T>) { return ccm::builtin::isgreater(x, y); }
-		else
-		{
-			return x > y;
-		}
+		else { return !ccm::isunordered(x, y) && (x > y); }
 	}
 
 	/**
 	 * @brief Checks if the first argument is greater than the second.
-	 * @tparam T Type of the left-hand side.
-	 * @tparam U Type of the right-hand side.
+	 * @tparam Arithmetic1 Arithmetic type of the left-hand side.
+	 * @tparam Arithmetic2 Arithmetic type of the right-hand side.
 	 * @param x Value of the left-hand side of the comparison.
 	 * @param y Value of the right-hand side of the comparison.
 	 * @return true if the first argument is greater than the second, false otherwise.
 	 */
-	template <typename T, typename U>
-	constexpr bool isgreater(T x, U y) noexcept
+	template <typename Arithmetic1, typename Arithmetic2, std::enable_if_t<std::is_arithmetic_v<Arithmetic1> && std::is_arithmetic_v<Arithmetic2>, bool> = true>
+	constexpr bool isgreater(Arithmetic1 x, Arithmetic2 y) noexcept
 	{
 		// Find the common type of the two arguments
-		using shared_type = std::common_type_t<T, U>;
+		using shared_type = std::common_type_t<Arithmetic1, Arithmetic2>;
 
 		// Then cast the arguments to the common type and call the single argument version
 		return static_cast<shared_type>(isgreater<shared_type>(static_cast<shared_type>(x), static_cast<shared_type>(y)));
--- include/ccmath/math/compare/isless.hpp
+++ include/ccmath/math/compare/isless.hpp
@@ -11,6 +11,7 @@
 #pragma once
 
 #include "ccmath/internal/math/generic/builtins/compare/isless.hpp"
+#include "ccmath/math/compare/isunordered.hpp"
 
 #include <type_traits>
 
@@ -23,29 +24,26 @@ namespace ccm
 	 * @param y A floating-point or integer value.
 	 * @return true if the first argument is less than the second, false otherwise.
 	 */
-	template <typename T>
+	template <typename T, std::enable_if_t<std::is_floating_point_v<T>, bool> = true>
 	constexpr bool isless(T x, T y) noexcept
 	{
 		if constexpr (ccm::builtin::has_constexpr_isless<T>) { return ccm::builtin::isless(x, y); }
-		else
-		{
-			return x < y;
-		}
+		else { return !ccm::isunordered(x, y) && (x < y); }
 	}
 
 	/**
 	 * @brief Checks if the first argument is less than the second.
-	 * @tparam T Type of the left-hand side.
-	 * @tparam U Type of the right-hand side.
+	 * @tparam Arithmetic1 Arithmetic type of the left-hand side.
+	 * @tparam Arithmetic2 Arithmetic type of the right-hand side.
 	 * @param x Value of the left-hand side of the comparison.
 	 * @param y Value of the right-hand side of the comparison.
 	 * @return true if the first argument is less than the second, false otherwise.
 	 */
-	template <typename T, typename U>
-	constexpr bool isless(T x, U y) noexcept
+	template <typename Arithmetic1, typename Arithmetic2, std::enable_if_t<std::is_arithmetic_v<Arithmetic1> && std::is_arithmetic_v<Arithmetic2>, bool> = true>
+	constexpr bool isless(Arithmetic1 x, Arithmetic2 y) noexcept
 	{
 		// Find the common type of the two arguments
-		using shared_type = std::common_type_t<T, U>;
+		using shared_type = std::common_type_t<Arithmetic1, Arithmetic2>;
 
 		// Then cast the arguments to the common type and call the single argument version
 		return static_cast<shared_type>(isless<shared_type>(static_cast<shared_type>(x), static_cast<shared_type>(y)));
