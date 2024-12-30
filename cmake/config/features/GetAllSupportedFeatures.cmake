5e6e546524a6a99b7d98dcb195b6073fa446c07f
Ian <ianpike98@gmail.com>
2024-12-29 20:31:59 -0500

    Remove builtin detection with cmake
    Signed-off-by: Ian <ianpike98@gmail.com>

--- cmake/config/features/GetAllSupportedFeatures.cmake
+++ cmake/config/features/GetAllSupportedFeatures.cmake
@@ -1,12 +1,5 @@
-# Common Support Builtins
-include(${CCMATH_SOURCE_DIR}/cmake/config/features/builtin/support/CheckBuiltinBitCastSupport.cmake)
-include(${CCMATH_SOURCE_DIR}/cmake/config/features/builtin/support/CheckBuiltinFmaSupport.cmake)
-include(${CCMATH_SOURCE_DIR}/cmake/config/features/builtin/math/compare/CheckBuiltinSignbitSupport.cmake)
-
-
-# TODO: Revisit this in a manner that is better. The current approach takes up too much time in the cmake.
-#include(${CCMATH_SOURCE_DIR}/cmake/config/features/CheckAllSupportedBuiltinFeatures.cmake)
+include(${CCMATH_SOURCE_DIR}/cmake/config/features/CheckAllSupportedBuiltinFeatures.cmake)
 
 if (CCMATH_ENABLE_RUNTIME_SIMD)
-  include(${CCMATH_SOURCE_DIR}/cmake/config/features/CheckAllSupportedSimdFeatures.cmake)
+    include(${CCMATH_SOURCE_DIR}/cmake/config/features/CheckAllSupportedSimdFeatures.cmake)
 endif ()
--- cmake/config/features/builtin/math/fmanip/CheckBuiltinScalbnSupport.cmake
+++
@@ -1,3 +0,0 @@
-include(CheckCXXSourceCompiles)
-
-# TODO: For now I'm not implementing checks for scalbn as checking it for constexpr is a little trickier.
