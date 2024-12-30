e34a56422050f14640d67ba7ea8f5dc0fc501ce3
Ian <ianpike98@gmail.com>
2024-12-29 21:00:46 -0500

    Finalize fma
    Signed-off-by: Ian <ianpike98@gmail.com>

--- include/ccmath/internal/math/generic/builtins/basic/fma.hpp
+++ include/ccmath/internal/math/generic/builtins/basic/fma.hpp
@@ -21,9 +21,9 @@
 /// - GCC 5.1+
 
 #ifndef CCMATH_HAS_CONSTEXPR_BUILTIN_FMA
-#if defined(__GNUC__) && (__GNUC__ > 5 || (__GNUC__ == 5 && __GNUC_MINOR__ >= 1)) && !defined(__clang__) && !defined(__NVCOMPILER_MAJOR__)
+	#if defined(__GNUC__) && (__GNUC__ > 5 || (__GNUC__ == 5 && __GNUC_MINOR__ >= 1)) && !defined(__clang__) && !defined(__NVCOMPILER_MAJOR__)
 		#define CCMATH_HAS_CONSTEXPR_BUILTIN_FMA
-#endif
+	#endif
 #endif
 
 namespace ccm::builtin
@@ -32,6 +32,14 @@ namespace ccm::builtin
 	template <typename T>
 	inline constexpr bool has_constexpr_fma =
 		#ifdef CCMATH_HAS_CONSTEXPR_BUILTIN_FMA
+			is_valid_builtin_type<T>;
+		#else
+			false;
+		#endif
+
+	template <typename T>
+	inline constexpr bool has_fma =
+		#if defined(__builtin_fma) || defined(__builtin_fmaf) || defined(__builtin_fmal)
 			is_valid_builtin_type<T>;
 		#else
 			false;
@@ -46,21 +54,28 @@ namespace ccm::builtin
 	 * This is thanks to taking advantage of ADL.
 	 */
 	template <typename T>
-	constexpr auto fma(T x, T y, T z)
-		-> std::enable_if_t<has_constexpr_fma<T>, T>
+	constexpr auto fma(T x, T y, T z) -> std::enable_if_t<has_constexpr_fma<T>, T>
 	{
-		if constexpr (std::is_same_v<T, float>)
-		{
-			return __builtin_fmaf(x, y, z);
-		}
-		if constexpr (std::is_same_v<T, double>)
-		{
-			return __builtin_fma(x, y, z);
-		}
-		if constexpr (std::is_same_v<T, long double>)
-		{
-			return __builtin_fmal(x, y, z);
-		}
+		if constexpr (std::is_same_v<T, float>) { return __builtin_fmaf(x, y, z); }
+		if constexpr (std::is_same_v<T, double>) { return __builtin_fma(x, y, z); }
+		if constexpr (std::is_same_v<T, long double>) { return __builtin_fmal(x, y, z); }
+		// This should never be reached
+		return T{};
+	}
+
+	/**
+	 * Wrapper for constexpr __builtin_fma.
+	 * This should be used internally and always be wrapped in an if constexpr statement.
+	 * It exists only to allow for usage of __builtin_fma without triggering a compiler error
+	 * when the compiler does not support it.
+	 * This is thanks to taking advantage of ADL.
+	 */
+	template <typename T>
+	auto fma(T x, T y, T z) -> std::enable_if_t<has_fma<T>, T>
+	{
+		if constexpr (std::is_same_v<T, float>) { return __builtin_fmaf(x, y, z); }
+		if constexpr (std::is_same_v<T, double>) { return __builtin_fma(x, y, z); }
+		if constexpr (std::is_same_v<T, long double>) { return __builtin_fmal(x, y, z); }
 		// This should never be reached
 		return T{};
 	}
