5aaae534a4d61040f288cbae23378f8a8ba3d71d
Ian <ianpike98@gmail.com>
2025-01-12 23:54:03 -0500

    Update supported simd abi
    Signed-off-by: Ian <ianpike98@gmail.com>

--- include/ccmath/internal/math/runtime/simd/func/pow.hpp
+++ include/ccmath/internal/math/runtime/simd/func/pow.hpp
@@ -20,28 +20,12 @@
 		#include "impl/sse2/pow.hpp"
 	#endif
 
-	#ifdef CCMATH_HAS_SIMD_SSE3
-		#include "impl/sse3/pow.hpp"
-	#endif
-
-	#ifdef CCMATH_HAS_SIMD_SSSE3
-		#include "impl/ssse3/pow.hpp"
-	#endif
-
 	#ifdef CCMATH_HAS_SIMD_SSE4
 		#include "impl/sse4/pow.hpp"
 	#endif
 
-	#ifdef CCMATH_HAS_SIMD_AVX
-		#include "impl/avx/pow.hpp"
-	#endif
-
 	#ifdef CCMATH_HAS_SIMD_AVX2
 		#include "impl/avx2/pow.hpp"
-	#endif
-
-	#ifdef CCMATH_HAS_SIMD_AVX512F
-		#include "impl/avx512/pow.hpp"
 	#endif
 
 // TODO: NEON does not have any builtin intrinsic for pow.
