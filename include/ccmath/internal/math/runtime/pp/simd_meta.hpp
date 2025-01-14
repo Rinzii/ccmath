6d83f9306b08015b5569c660c35d441c6a983262
Ian <ianpike98@gmail.com>
2025-01-14 03:50:48 -0500

    Initial work on adding new simd support
    Signed-off-by: Ian <ianpike98@gmail.com>

--- include/ccmath/internal/math/runtime/pp/simd_meta.hpp
+++ include/ccmath/internal/math/runtime/pp/simd_meta.hpp
@@ -2,22 +2,23 @@
 #pragma once
 
 #include "fwddecl.hpp"
-//#include "flags.hpp"
+// #include "flags.hpp"
 
 namespace ccm::pp::detail
 {
 	template <typename T>
-struct assert_unreachable
+	struct assert_unreachable
 	{
 		static_assert(!std::is_same_v<T, T>, "this should be unreachable");
 	};
 
-
 	template <typename Abi, typename = void>
-struct is_simd_abi_tag : std::false_type {};
+	struct is_simd_abi_tag : std::false_type
+	{
+	};
 
 	template <typename Abi>
-	struct is_simd_abi_tag<Abi, std::void_t<
-		decltype(!Abi::template IsValid<void>::value),
-		typename Abi::IsValidAbiTag>> : std::true_type {};
-}
+	struct is_simd_abi_tag<Abi, std::void_t<decltype(!Abi::template IsValid<void>::value), typename Abi::IsValidAbiTag>> : std::true_type
+	{
+	};
+} // namespace ccm::pp::detail
