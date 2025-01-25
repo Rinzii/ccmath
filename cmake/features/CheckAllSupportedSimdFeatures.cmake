734c4902365a7ebd771ad09761de6ac28ecb3de6
Ian <ianpike98@gmail.com>
2025-01-24 19:50:03 -0500

    Fix incorrect paths
    Signed-off-by: Ian <ianpike98@gmail.com>

--- cmake/features/CheckAllSupportedSimdFeatures.cmake
+++ cmake/features/CheckAllSupportedSimdFeatures.cmake
@@ -2,8 +2,8 @@ if (NOT DEFINED CCMATH_ROOT_DIR)
     message(FATAL_ERROR "CCMATH_ROOT_DIR is not defined. Did you forget to include the main CMakeLists.txt?")
 endif ()
 
-include(${CCMATH_ROOT_DIR}/cmake/config/features/simd/CheckFMASupport.cmake)
+include(${CCMATH_ROOT_DIR}/cmake/features/simd/CheckFMASupport.cmake)
 
 if (NOT CCMATH_DISABLE_SVML_USAGE)
-    include(${CCMATH_ROOT_DIR}/cmake/config/features/simd/CheckSVMLSupport.cmake)
+    include(${CCMATH_ROOT_DIR}/cmake/features/simd/CheckSVMLSupport.cmake)
 endif ()
