946adcc121cdc1bf7e1fbb17b6a6165049e4461f
ianpike <ianpike98@gmail.com>
2026-06-05 13:40:01 -0400

    Make sure signed zero behavior is preserved for IEC-60559

--- include/ccmath/internal/math/generic/func/basic/max_gen.hpp
+++ include/ccmath/internal/math/generic/func/basic/max_gen.hpp
@@ -42,6 +42,10 @@ namespace ccm::gen
 			if (CCM_UNLIKELY(x_is_nan)) { return y; }
 
 			if (CCM_UNLIKELY(y_is_nan)) { return x; }
+
+			// Preserve IEC 60559 signed-zero behavior for fmax: if both are zero,
+			// return +0 when either argument is +0.
+			if (CCM_UNLIKELY(x_bits.is_zero() && y_bits.is_zero())) { return x_bits.is_neg() ? y : x; }
 		}
 
 		return (x > y) ? x : y;
