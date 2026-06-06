1748aaa3df8ccbea62c02b8a2bd1857556cfee24
ianpike <ianpike98@gmail.com>
2026-06-06 00:11:09 -0400

    Fix ensure_format.sh execution in lint CI

--- tools/run_format.sh
+++ tools/run_format.sh
@@ -2,4 +2,4 @@
 # Check header formatting and apply clang-format only when needed.
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-"${SCRIPT_DIR}/ensure_format.sh"
+bash "${SCRIPT_DIR}/ensure_format.sh"
