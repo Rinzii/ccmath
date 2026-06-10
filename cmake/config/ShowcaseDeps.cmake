58bbc35dcc6672acbf0c550c9f6648c7ca384222
ianpike <ianpike98@gmail.com>
2026-06-10 15:56:56 -0400

    Optimize our CI run times

--- cmake/config/ShowcaseDeps.cmake
+++ cmake/config/ShowcaseDeps.cmake
@@ -15,6 +15,7 @@ function(ccmath_showcase_fetch_gcem)
             gcem
             GIT_REPOSITORY https://github.com/kthohr/gcem.git
             GIT_TAG ${_CCMATH_GCEM_VERSION}
+            GIT_SHALLOW TRUE
     )
     FetchContent_GetProperties(gcem)
     if (NOT gcem_POPULATED)
@@ -45,6 +46,7 @@ function(ccmath_showcase_fetch_gtest)
             googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
             GIT_TAG ${_CCMATH_GTEST_VERSION}
+            GIT_SHALLOW TRUE
     )
     FetchContent_MakeAvailable(googletest)
 
