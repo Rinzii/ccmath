2f1f5b29945d772d0b93a847db15a5dd2aa763fc
ianpike <ianpike98@gmail.com>
2026-06-12 23:18:53 -0400

    Add missing final double log table entry with regression tests

--- tests/src/math/expo/log10_test.cpp
+++ tests/src/math/expo/log10_test.cpp
@@ -9,6 +9,8 @@
  */
 
 #include "ccmath/ccmath.hpp"
+#include "ccmath/internal/math/generic/func/expo/log10_gen.hpp"
+#include "utils/test_runtime.hpp"
 #include "utils/ulp_suite.hpp"
 
 #include <gtest/gtest.h>
@@ -26,4 +28,20 @@ TEST(CcmathExponentialTests, Log10)
 
 	ccm::test::ExpectUlpUnaryVsStd(2.0, ccm::log10<double>, static_cast<double (*)(double)>(std::log10));
 	ccm::test::ExpectUlpUnaryVsStd(100.0, ccm::log10<double>, static_cast<double (*)(double)>(std::log10));
+}
+
+TEST(CcmathExponentialTests, Log10DoubleLastTableBucket)
+{
+	// log10 derives from the double log kernel, so the missing final log table
+	// entry broke the same significand range. Pin the failing bit patterns.
+	static_assert(ccm::log10(0x1.5fdffffffff32p+0) == 0x1.1aed92aadd55p-3, "log10 must use the final log table entry in the last bucket");
+
+	constexpr double inputs[] = {
+		0x1.5fdffffffff32p-1011, 0x1.5fdffffffff32p-1, 0x1.5fdffffffff32p+0, 0x1.5fdffffffff32p+1,
+		0x1.5ep+0,				 0x1.5ffffffffffffp+0, 0x1.5ep-512,			 0x1.5ffffffffffffp+512,
+	};
+	for (double input : inputs)
+	{
+		ccm::test::ExpectUlpUnaryVsStd(ccm::test::runtime_value(input), ccm::gen::log10_gen<double>, static_cast<double (*)(double)>(std::log10));
+	}
 }
